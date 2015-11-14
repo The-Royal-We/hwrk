0b33c291343dd5b393721f444ff565a961f13874
The-Royal-We <brendan.carroll.2012@nuim.ie>
2015-11-14 18:33:35 +0000

    Added more stuff to assignment

--- 11348006_Assignment1.scm
+++ 11348006_Assignment1.scm
@@ -1,11 +1,33 @@
 #lang racket
 ;;(free-variables '((a b) (λ c ((d c) (e b))))) ⇒ (a b d e)
+;; If there's a symbol followed by a λ then drop it out
+;; If there's a list followed by a λ then drop it out
+;; Otherwise just return the list with all of the variables
+
+
+;;> (cons (car '(λ c ((d c) (e b)))) (cadr '(λ c ((d c) (e b))) ))
+;;'(λ . c)
+;;> (cons (car '(λ c ((d c) (e b)))) (cadr '(λ (c b) ((d c) (e b))) ))
+;;'(λ c b)
 
 (define free-variables (λ (exp)
                          (cond [(empty? exp) '()]
-                               [(list? (car exp))
-                                (cond [(not (member 'λ (car exp)))
-                                       (cons (car exp) (free-variables (cadr exp)))]
-                                      [else ( free-variables (caddr exp))])]
-                         [else (cond [(equal? (car exp) 'λ) '()]
-                                     [else (cons (car exp) '())])])))
+                               [(list? exp)
+                                (cond [(member (car exp) '(λ))
+                                       (free-variables-a (cdr exp) (cadr exp))]
+                                      [else (cons (car exp) (free-variables (cadr exp)))])]
+                         [else (cond [(equal? exp 'λ) '()]
+                                     [else (cons exp '())])])))
+
+ (define free-variables-a (λ (exp bounded) ()))
+
+(define (flatmap f list)
+  apply append (map f list))
+
+(define (flatten x)
+    (cond ((null? x) '())
+          ((not (pair? x)) (list x))
+          (else (append (flatten (car x))
+                        (flatten (cdr x))))))
+
+(flatmap (λ (xs) (filter (λ (exp) (not (equal? 'λ exp))) xs)) '((a b) (λ c ((d c) (e b)))) )
